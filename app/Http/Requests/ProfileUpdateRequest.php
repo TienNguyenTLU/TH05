7097f9958263dfa844caf3e99438549b88ca8018
Tien Nguyen <tiencp1234hd@gmail.com>
2024-12-30 15:13:22 +0700

    Fixed layouts

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,9 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'bio'=> ['required', 'string'],
+            'avatar' => ['nullable', 'image', 'max:1024', 'mimes:jpeg,png,jpg,gif,svg,webp'],
         ];
+
     }
 }
